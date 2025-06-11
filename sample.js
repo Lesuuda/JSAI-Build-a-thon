81cdf29e4281d88d5e3ab579a4e5fdce3108b320
lesuuda <lesuudaljamani@gmail.com>
2025-06-11 15:36:13 +0300

    Working with GitHub Models and AI Toolkit

--- sample.js
+++ sample.js
@@ -1,10 +1,14 @@
 import ModelClient, { isUnexpected } from "@azure-rest/ai-inference";
 import { AzureKeyCredential } from "@azure/core-auth";
 import "dotenv/config";
-
 const token = process.env["GITHUB_TOKEN"];
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 const endpoint = "https://models.github.ai/inference";
-const model = "deepseek/DeepSeek-V3-0324";
+const model = "openai/gpt-4.1";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export async function main() {
 
@@ -12,16 +16,31 @@ export async function main() {
     endpoint,
     new AzureKeyCredential(token),
   );
-
+  // read image as base64
+  const imagePath = resolve(__dirname, "contoso_layout_sketch.jpg");
+  const image = fs.readFileSync(imagePath)
+  const base64Image = image.toString("base64");
   const response = await client.path("/chat/completions").post({
     body: {
       messages: [
         { role:"system", content: "You are a helpful assistant." },
-        { role:"user", content: "What is the capital of France?" }
+        { role:"user",
+          content: [
+            {
+              type: "text",
+              text: "Generate a web page layout based on the provided sketch."
+            },
+            {
+              type: "image_url",
+              image_url: {
+                url: `data:image/jpeg;base64,${base64Image}`
+              }
+            }
+          ]}
       ],
-      temperature: 1.0,
+      temperature: 0.2,
       top_p: 1.0,
-      max_tokens: 1000,
+      max_tokens: 2048,
       model: model
     }
   });
