73fd2c151c484d48df8483ab450dc9d93c4cab5b
lesuuda <lesuudaljamani@gmail.com>
2025-06-23 17:21:52 +0300

    Connect to Azure AI Foundry

--- ai-foundry.js
+++ ai-foundry.js
@@ -1,27 +1,25 @@
 import ModelClient from "@azure-rest/ai-inference";
 import { AzureKeyCredential } from "@azure/core-auth";
 import dotenv from "dotenv";
-
 dotenv.config();
-const client = ModelClient(
-    process.env.AZURE_INFERENCE_SDK_ENDPOINT,
-    new AzureKeyCredential(process.env.AZURE_INFERENCE_SDK_KEY)
-);
-console.log(process.env.AZURE_INFERENCE_SDK_ENDPOINT);
-var messages = [
-    {role: "system", content: "You are a helpful assistant."},
-    {role: "user", content: "What is self awareness?"}
+  const client = new ModelClient(
+  process.env.AZURE_INFERENCE_SDK_ENDPOINT, new AzureKeyCredential(process.env.AZURE_INFERENCE_SDK_KEY));
 
+var messages = [
+  { role: "system", content: "You are an helpful assistant" },
+  { role: "user", content: "What are 3 things to see in Seattle?" },
 ];
 
-var response = await client.path("/chat/completions").post({
-    body: {
-        messages:messages,
-        max_tokens: 4096,
-        temperature: 1,
-        top_p: 1.0,
-        model: "gpt-4.1",
-    
-    },
+var response = await client.path("chat/completions").post({
+  body: {
+    messages: messages,
+    max_tokens: 2048,
+      temperature: 0.8,
+      top_p: 0.1,
+      presence_penalty: 0,
+      frequency_penalty: 0,
+      model: "Llama-4-Maverick",
+  },
 });
-console.log(response.body.choices[0].message.content);
+
+console.log(JSON.stringify(response));
--- sample.js
+++ sample.js
@@ -1,14 +1,9 @@
 import ModelClient, { isUnexpected } from "@azure-rest/ai-inference";
 import { AzureKeyCredential } from "@azure/core-auth";
-import "dotenv/config";
+
 const token = process.env["GITHUB_TOKEN"];
-import fs from "fs";
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
 const endpoint = "https://models.github.ai/inference";
-const model = "openai/gpt-4.1";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const model = "meta/Llama-4-Maverick-17B-128E-Instruct-FP8";
 
 export async function main() {
 
@@ -16,31 +11,16 @@ export async function main() {
     endpoint,
     new AzureKeyCredential(token),
   );
-  // read image as base64
-  const imagePath = resolve(__dirname, "contoso_layout_sketch.jpg");
-  const image = fs.readFileSync(imagePath)
-  const base64Image = image.toString("base64");
+
   const response = await client.path("/chat/completions").post({
     body: {
       messages: [
         { role:"system", content: "You are a helpful assistant." },
-        { role:"user",
-          content: [
-            {
-              type: "text",
-              text: "Generate a web page layout based on the provided sketch."
-            },
-            {
-              type: "image_url",
-              image_url: {
-                url: `data:image/jpeg;base64,${base64Image}`
-              }
-            }
-          ]}
+        { role:"user", content: "What is the capital of France?" }
       ],
-      temperature: 0.2,
+      temperature: 1.0,
       top_p: 1.0,
-      max_tokens: 2048,
+      max_tokens: 1000,
       model: model
     }
   });
